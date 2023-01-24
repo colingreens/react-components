c3ec45beac01c20066812ac62f30126e3c778d6f
colin_greens <colin.j.benson@gmail.com>
2023-01-23 20:30:19 -0800

    moved todo to component ish

---
+++ components/DragAndDrop/Todo.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import { DragDropContext } from "react-beautiful-dnd";
+import styled from "styled-components";
+import Column from "./Column";
+import initialData from "./initial-data";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+export default function Todo() {
+  const [state, setState] = useState(initialData);
+
+  function onDragEnd(result) {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) return;
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const start = state.columns[source.droppableId];
+    const finish = state.columns[destination.droppableId];
+
+    //moving within the same column
+    if (start === finish) {
+      const newTaskIds = Array.from(start.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...start,
+        taskIds: newTaskIds,
+      };
+
+      const newState = {
+        ...state,
+        columns: {
+          ...state.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+
+      setState(newState);
+      return;
+    }
+    //moving to another column
+    const startTaskIds = Array.from(start.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds,
+    };
+    const finishTaskIds = Array.from(finish.taskIds);
+    finishTaskIds.splice(destination.index, 0, draggableId);
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds,
+    };
+
+    const newState = {
+      ...state,
+      columns: {
+        ...state.columns,
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish,
+      },
+    };
+    setState(newState);
+  }
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Container>
+        {state.columnOrder.map((columnId) => {
+          const column = state.columns[columnId];
+          const tasks = column.taskIds.map((taskId) => state.tasks[taskId]);
+
+          return <Column key={column.id} column={column} tasks={tasks} />;
+        })}
+      </Container>
+    </DragDropContext>
+  );
+}
